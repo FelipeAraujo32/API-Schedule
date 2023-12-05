231ca8c399544d2136ec4f297cdecf1f11b50b16
FelipeAraujo32 <felipecafsx@gmail.com>
2023-12-05 17:49:56 -0300

    nada

--- agenda/src/main/java/microservice/com/agenda/Entities/Paciente.java
+++ agenda/src/main/java/microservice/com/agenda/Entities/Paciente.java
@@ -1,14 +1,10 @@
 package microservice.com.agenda.Entities;
 
-import java.util.List;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,19 +14,12 @@ public class Paciente{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
     private String nome;
-    @Column(nullable = false)
     private String sobrenome;
-    @Column(nullable = false)
     private String cpf;
-    @Column(nullable = false)
     private String email;
 
-    @OneToMany(mappedBy = "paciente", fetch = FetchType.LAZY)
-    private List<Agenda> agendas;
 
-    @Deprecated
     public Paciente() {
     }
 
